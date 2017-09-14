4fa49ccf29c9eb706421c19866f8492f7749d95d
danstepanov <danhstepanov@gmail.com>
2017-09-14 13:51:16 -0400

    add ui components, add routing, add authentication, add ability to create links, add association of links to users

--- src/index.js
+++ src/index.js
@@ -1,8 +1,42 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { BrowserRouter } from 'react-router-dom'
+import {
+  ApolloProvider,
+  createNetworkInterface,
+  ApolloClient
+} from 'react-apollo'
+import App from './components/App'
+import registerServiceWorker from './registerServiceWorker'
+import './styles/index.css'
+import { GC_AUTH_TOKEN } from './constants'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+
+const networkInterface = createNetworkInterface({
+  uri: 'https://api.graph.cool/simple/v1/cj7jmvhsk08g001569gnwdhhg'
+})
+
+networkInterface.use([{
+  applyMiddleware(req, next) {
+    if (!req.options.headers) {
+      req.options.headers = {}
+    }
+    const token = localStorage.getItem(GC_AUTH_TOKEN)
+    req.options.headers.authorization = token ? `Bearer ${token}` : null
+    next()
+  }
+}])
+
+const client = new ApolloClient({
+  networkInterface
+})
+
+ReactDOM.render(
+  <BrowserRouter>
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+  </BrowserRouter>
+  , document.getElementById('root')
+)
+registerServiceWorker()
